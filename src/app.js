5dde7aa1eb0ce5c400c8539d464388c7454ed426
Łukasz Sałajczyk <lukasz.salajczyk@jazzy.pro>
2015-09-25 10:58:14 +0200

    all in app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,69 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('qul.env', []);
+  angular.module('qul.env', [])
+    .provider('Env', function () {
+      /**
+       *
+       * @type {{}}
+       */
+      this.config = {};
+
+      /**
+       *
+       * @param config
+       *
+       *
+       * @example
+       * <pre>
+       * angular.module('app', ['qul.env'])
+       *  .config(function(EnvProvider){
+       *      EnvProvider.setConfig({
+       *          'dev': {
+       *              match: 'localhost',
+       *              constants: {
+       *                  API_URL: 'http://localhost:3001'
+       *              }
+       *          },
+       *          'test': {
+       *              match: '.dev.jazzy.pro',
+       *              constants: {
+       *                  API_URL: 'http://api.dev.example.com'
+       *              }
+       *          },
+       *          'prod': {
+       *              match: true,
+       *              constants: {
+       *                  API_URL: 'http://api.example.com'
+       *              }
+       *          }
+       *      });
+       *  })
+       *  .service('Foo', function(Env){
+       *      console.log(Env.API_URL);
+       *  })
+       * </pre>
+       */
+      this.setConfig = function (config) {
+        this.config = config;
+      };
+
+      /**
+       *
+       * @returns {{}}
+       */
+      this.$get = function ($location) {
+        var env = (function (config, url) {
+          for (var name in config) {
+            if (true === new RegExp(config[name].match).test(url)) {
+              return name;
+            }
+          }
+
+          return 'prod';
+        }(this.config, $location.absUrl()));
+
+        return this.config[env].constants;
+      }
+    });
+}());
